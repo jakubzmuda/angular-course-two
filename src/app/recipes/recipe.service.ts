ab2fb5db789248213848ecc3deae92e4296c5b92
Jakub Żmuda <goodguykuba@gmail.com>
2017-09-29 15:38:56 +0200

    ingredients and unicorns

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is simply a test', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
-    new Recipe('Another  test recipe', 'this is simply an another test', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg')
+    new Recipe(
+      'Tasty schnitzel',
+      'good stuff',
+      'http://www.dvo.com/link/0630_155947172.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French fries', 20)
+      ]),
+    new Recipe(
+      'Big fat burger',
+      'oh whatever, really',
+      'http://aht.seriouseats.com/images/20100331-fatburger-large.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
 
   getRecipes(): Recipe[] {
