48bc57690600c28523a94306fce23610e4676a58
Jakub Żmuda <goodguykuba@gmail.com>
2017-09-29 15:04:15 +0200

    events through a service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is simply a test', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
     new Recipe('Another  test recipe', 'this is simply an another test', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg')
@@ -9,4 +12,5 @@ export class RecipeService {
   getRecipes(): Recipe[] {
     return this.recipes.slice();
   }
+
 }
